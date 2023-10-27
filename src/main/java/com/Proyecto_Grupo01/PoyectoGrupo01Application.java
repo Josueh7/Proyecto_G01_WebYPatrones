d770c97c0ffb143e544a75c3dca38826e9c22d2f
Josueh7 <josueh070499@gmail.com>
2023-10-27 17:55:25 -0600

    Creacion Proyecto

---
+++ src/main/java/com/Proyecto_Grupo01/PoyectoGrupo01Application.java
@@ -0,0 +1,13 @@
+package com.Proyecto_Grupo01;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PoyectoGrupo01Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PoyectoGrupo01Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/Proyecto_Grupo01/PoyectoGrupo01ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.Proyecto_Grupo01;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PoyectoGrupo01ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
